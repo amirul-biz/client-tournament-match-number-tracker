38ae5b72bb8022c3b2ade77917e0a98f5e5229a5
amirul.azreem <amirul.azreem@tranglo.com>
2025-11-01 22:30:42 +0800

    main
    - fix tailwind issue
    - Add base layout component
    - Install angular material

---
+++ src/app/layout-component/layout-component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout-component',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './layout-component.html',
+  styleUrl: './layout-component.scss',
+})
+export class LayoutComponent {
+
+}
