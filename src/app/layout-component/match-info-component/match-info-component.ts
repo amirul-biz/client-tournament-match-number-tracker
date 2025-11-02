d99b30ac451f931042529c7dfc2328f74afb0501
amirul.azreem <amirul.azreem@tranglo.com>
2025-11-02 11:49:39 +0800

    development
    - Design base match information card

--- src/app/layout-component/match-info-component/match-info-component.ts
+++ src/app/layout-component/match-info-component/match-info-component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatchInfoDockComponent } from './match-info-dock-component/match-info-dock-component';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-match-info-component',
-  imports: [CommonModule, MatchInfoDockComponent],
+  imports: [CommonModule, MatchInfoDockComponent, MatCardModule, MatIconModule],
   templateUrl: './match-info-component.html',
   styleUrl: './match-info-component.scss',
 })
