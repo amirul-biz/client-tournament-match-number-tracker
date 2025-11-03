1543f5a0caf68c07c443dcba3c227ef6eb586681
amirul.azreem <amirul.azreem@tranglo.com>
2025-11-03 22:36:38 +0800

    development
    - restructure display match UI
    - restructure dock componnet naming and instance

--- src/app/layout-component/match-info-component/match-info-component.ts
+++ src/app/layout-component/match-info-component/match-info-component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { MatchInfoDockComponent } from './match-info-dock-component/match-info-dock-component';
+import { DockComponent } from './dock-component/dock-component';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-match-info-component',
-  imports: [CommonModule, MatchInfoDockComponent, MatCardModule, MatIconModule],
+  imports: [CommonModule, DockComponent, MatCardModule, MatIconModule],
   templateUrl: './match-info-component.html',
   styleUrl: './match-info-component.scss',
 })
