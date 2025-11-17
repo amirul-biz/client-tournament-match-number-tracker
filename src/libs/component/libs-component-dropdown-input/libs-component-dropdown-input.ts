03817b6adc2584f146649f68459da578e9009bd9
amirul.azreem <amirul.azreem@tranglo.com>
2025-11-17 22:43:35 +0800

    development
    - Set mat form field display options to fill
    - Npm Install mat select search
    - Create shared text input form compinent
    - Create shared text search dropdown component

---
+++ src/libs/component/libs-component-dropdown-input/libs-component-dropdown-input.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormField } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+
+
+@Component({
+  selector: 'app-libs-component-dropdown-input',
+ imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    NgxMatSelectSearchModule,
+    MatIconModule,
+    MatTooltipModule,
+    
+  ],  templateUrl: './libs-component-dropdown-input.html',
+  styleUrl: './libs-component-dropdown-input.scss',
+})
+export class LibsComponentDropdownInput {
+@Input()
+  inputField: FormControl = new FormControl();
+  @Input() dataList: { [key: string]: any }[] = []
+  @Input() targetValueKey!: string
+  @Input() targetDisplayKey!: string
+  @Input()
+  labelText?: string;
+  @Input()
+  placeholder?: string;
+  @Input()
+  hint?: string;
+  @Input()
+  patternMessage?: string;
+  @Input() showInfoIcon: boolean = false;
+  @Input() tooltipText: string = '';
+  @Input() isMandatory: boolean = true;
+}
