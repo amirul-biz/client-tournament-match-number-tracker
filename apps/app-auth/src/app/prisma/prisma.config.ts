a04413e08803f33c7dc506584b88896956fc5144
Amirul Irfan <amirul.irfan.biz@gmail.com>
2025-12-08 16:03:45 +0800

    development
    - set up prisma for auth service
    - restructure scripts for auth
    - add user initial script migration

---
+++ apps/app-auth/src/app/prisma/prisma.config.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'
+import { defineConfig, env } from "prisma/config";
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "migrations",
+  },
+  datasource: {
+    url: env("AUTH_DATABASE_URL"),
+  },
+});
